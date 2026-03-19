1cd67c0e24707f031f53e4325afc5fa3a25b4190
ChSPN <charlene.scomparin@gmail.com>
2026-03-19 12:35:52 +0100

    fix: hardcode API URL for production

--- monorepo-chat/web/src/App.jsx
+++ monorepo-chat/web/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
+const socket = io("https://chat-api-eepo.onrender.com", {
   transports: ["websocket"],
 });
 
